b1c5104c015fa49a1df508d43c4f582e6b3e56eb
DinithKumudika <dinithwalpitagama@gmail.com>
2023-03-05 21:34:05 +0530

    init

---
+++ lib/components/FloatingButton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+//https://api.flutter.dev/flutter/material/Icons-class.html
+
+class FloatingButton extends StatelessWidget {
+  final IconData iconData;
+  final Function() onPressed;
+  final String tooltipText;
+  final Color iconColor;
+  final Color backgroundColor;
+
+  FloatingButton({
+    required this.tooltipText,
+    required this.iconData,
+    required this.iconColor,
+    required this.backgroundColor,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      tooltip: tooltipText,
+      backgroundColor: backgroundColor,
+      child: Icon(
+        iconData,
+        color: iconColor,
+      ),
+    );
+  }
+}
