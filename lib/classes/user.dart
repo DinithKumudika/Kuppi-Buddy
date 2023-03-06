7559e6d14291ab714cd2f781e60764cb61d51531
DinithKumudika <dinithwalpitagama@gmail.com>
2023-03-06 15:27:20 +0530

    rounded button widget

--- lib/classes/user.dart
+++ lib/classes/user.dart
@@ -1,4 +1,15 @@
 // ignore: file_names
 class User {
+  late String username;
+  late String password;
 
+  bool login(String username, String password) {
+    bool status=false;
+    return status;
+  }
+
+  bool register(String username, String password) {
+    bool status=false;
+    return status;
+  }
 }
