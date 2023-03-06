78faa75372223c95b811d5d5e2f0f370f1b6b7f7
DinithKumudika <dinithwalpitagama@gmail.com>
2023-03-06 14:56:28 +0530

    theme colors defined

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+// theme - colors
+
+const COLOR_PRIMARY = Color.fromARGB(255, 39, 87, 200);
+const COLOR_PRIMARY_LIGHT = Color.fromARGB(255, 15, 164, 222);
+const COLOR_SECONDARY = Color(0xFF79DAE8);
+const COLOR_SECONDARY_LIGHT = Color(0xFFE8F9FD);
+const COLOR_LIGHT = Colors.white;
